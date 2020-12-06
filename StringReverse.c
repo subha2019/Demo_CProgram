965dd9019aa851a78cf79838e3e10102f71aa557
subha2019 <65356460+subha2019@users.noreply.github.com>
2020-12-06 13:40:39 +0530

    Update StringReverse.c

--- StringReverse.c
+++ StringReverse.c
@@ -2,21 +2,22 @@
 #include<string.h>
 int main()
 {
-  //char str[100]="NIAWS TIHSRAD\n", strrev[100];
+  char str[100]="NIAWS TIHSRAD", strrev[100],temp[100];;
   printf("\nEnter the string to be reverse");
-  //int i,j;
-  //while(str[i]!='\n')
+  int i,j;
+  j=strlen(str);
+  //while(str[i]!='\0')
   //{
     // j++;
   //}
   //printf("%d",j);
-  /*i=0;
+  i=0;
+  j=j-1;
   for(i=0;i<j;i++)
   {
     strrev[i]=str[j];
-    i++;
     j--;
-  }*/
+  }
  
 }
 
