ef56d76371eccbc8530d794a64a3c748763c5b58
subha2019 <65356460+subha2019@users.noreply.github.com>
2020-12-06 12:25:34 +0530

    Create StringReverse.c

---
+++ StringReverse.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  char str[100], strrev[100];
+  printf("\nEnter the string to be reverse");
+  str[100]="NIAWS TIHSRAD\n";
+  int i,j;
+  while(str[i]!=\n)
+  {
+     j++;
+  }
+  printf(%d",j);
+  i=0;
+  for(i=0;i<j;i++
+  {
+    strrev[i]=str[j];
+    i++;
+    j--;
+  }
+ 
+}
+
+
+
